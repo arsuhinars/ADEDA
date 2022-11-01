0dd0bbff5b31b4cd96adad9778d591a544278b52
arsuhinars <arsuhinars@gmail.com>
2022-11-01 09:39:52 +0400

    Updated config fields

--- app/config.py
+++ app/config.py
@@ -1,7 +1,15 @@
 from os import environ
 
 DATABASE_URL = environ.get('DATABASE_URL')
+SECRET_KEY = environ.get('SECRET_KEY')
+
+ADMIN_LOGIN = environ.get('ADMIN_LOGIN')
+ADMIN_PASSWORD = environ.get('ADMIN_PASSWORD')
+
+ACCESS_TOKEN_EXPIRE = 30 * 60
 
 CACHE_SETTINGS = {
     'DEFAULT_TIMEOUT': 300
 }
+
+PASSWORD_SALT_LENGTH = 16
