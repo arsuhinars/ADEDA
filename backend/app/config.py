64cd99b9b1d65a5abb01f4cb686fad781d6ecb09
arsuhinars <arsuhinars@gmail.com>
2022-11-03 01:48:23 +0400

    config.py minor update

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,19 +1,19 @@
 from os import environ
 
+# Настройки БД
 DATABASE_URL = environ.get('DATABASE_URL')
 SECRET_KEY = environ.get('SECRET_KEY')
+
+
+# Настройки авторизации
+ACCESS_TOKEN_EXPIRE = 30 * 60
+PASSWORD_SALT_LENGTH = 16
 
 ADMIN_LOGIN = environ.get('ADMIN_LOGIN')
 ADMIN_PASSWORD = environ.get('ADMIN_PASSWORD')
 
-ACCESS_TOKEN_EXPIRE = 30 * 60
 
-CACHE_SETTINGS = {
-    'DEFAULT_TIMEOUT': 300
-}
-
-PASSWORD_SALT_LENGTH = 16
-
+# Настройки парсера
 PARSE_DELAY = 10
 PARSER_BROWSER = 'firefox'
 WEBDRIVER_OPTIONS = {
