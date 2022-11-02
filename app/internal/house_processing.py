0022a3c8f4acd7a4b4d7cbdbdab1b2e0bd3afd7f
arsuhinars <arsuhinars@gmail.com>
2022-11-02 10:52:17 +0400

    Added house_proessing.py template

---
+++ app/internal/house_processing.py
@@ -0,0 +1,13 @@
+from ..schemas import House, HouseAnalogAdjustments
+
+def calculate_adjustments(
+    reference: House,
+    analog: House
+    ) -> HouseAnalogAdjustments:
+    """
+    Функция вычисления корректировок для аналога и эталона
+
+    * `reference` - объект дома - эталона
+    * `analog` - объект аналога
+    """
+    pass
