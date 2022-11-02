5cdfe4021b0a2f3038e53bcbde0cea946a402442
arsuhinars <arsuhinars@gmail.com>
2022-11-03 00:16:37 +0400

    Updated user model

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,11 +1,7 @@
 from pydantic import BaseModel
 
 class UserCreate(BaseModel):
+    """ Схема, используемая для создания пользователя """
     login: str
     password: str
     is_admin = False
-
-
-class UserBase(BaseModel):
-    id: int
-    login: str
