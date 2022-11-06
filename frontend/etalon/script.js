40477b63eb9f30af19b74784e28520b7fa68f925
arsuhinars <arsuhinars@gmail.com>
2022-11-06 23:08:29 +0400

    Added scripting functionality to etalon

--- frontend/etalon/script.js
+++ frontend/etalon/script.js
@@ -9,10 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < references.length; i++) {
         let ref_house = references[i]
         
-        html += '<p><input name="model" type="radio" value="">'
+        html += '<p><input name="model" type="radio" value="' + i + '">'
         html += ref_house.location + ' (' + ref_house.floor + ' этаж из ' + ref_house.floors_count + ')'
         html += '</input></p>'
 
         etalons.innerHTML = html + etalons.innerHTML
     }
+
+    document.querySelector('#select-btn').addEventListener('click', () => {
+        let index = document.querySelector('input[type=radio]:checked').value
+
+        sessionStorage.setItem('curr_ref', JSON.stringify(references[index]))
+
+        window.location.href = '/analog'
+    })
 })
